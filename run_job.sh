82167c95909939f41d2384842e734daab9bc1772
ZenoTsT <167864205+ZenoTsT@users.noreply.github.com>
2025-11-18 11:09:16 +0100

    uograded run_job

--- run_job.sh
+++ run_job.sh
@@ -15,7 +15,6 @@ docker run --gpus all --rm \
     -e HOME=/$USER \
     nvcr.io/nvidia/pytorch:21.02-py3 \
     bash -lc "pip install -r qwen3-vl-slt-how2sign/requirements.txt && \
-              cd qwen3-vl-slt-how2sign/scripts && \
-              python smoke_test_qwen3vl.py"
+              python qwen3-vl-slt-how2sign/scripts/smoke_test_qwen3vl.py"
 
 stop_rootless_docker.sh
